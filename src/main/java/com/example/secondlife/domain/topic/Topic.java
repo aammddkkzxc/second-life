ecf9bcfe92f5a890356689dfdc32943cf9be6b15
aammddkkzxc <ideal0427@naver.com>
2024-03-26 23:04:28 +0900

    feat: 토픽 테이블형

---
+++ src/main/java/com/example/secondlife/domain/topic/Topic.java
@@ -0,0 +1,32 @@
+package com.example.secondlife.domain.topic;
+
+import com.example.secondlife.domain.post.entity.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
