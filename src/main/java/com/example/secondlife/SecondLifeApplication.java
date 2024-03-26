6cf0118fe8c60c0f3a65a39c6152098017df8ba5
aammddkkzxc <ideal0427@naver.com>
2024-03-26 13:48:01 +0900

    feat: 프로젝트 구조 초기 설정

---
+++ src/main/java/com/example/secondlife/SecondLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/secondlife/SecondLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
