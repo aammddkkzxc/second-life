c78babf8722ff4ff26f66880e1c96a37383e1c5f
js <2kpppk@gmail.com>
2024-03-27 17:42:09 +0900

    feat : #15 UserController, UserService 테스트코드 수정..

--- src/test/java/com/example/secondlife/domain/user/service/UserServiceTest.java
+++ src/test/java/com/example/secondlife/domain/user/service/UserServiceTest.java
@@ -44,12 +44,13 @@ class UserServiceTest {
     void getUserInfo() {
         JoinResponse joinResponse = saveMember();
 
-        UserInfo userInfo = userService.getUserInfo(joinResponse.getUserId());
+        userService.getUserInfo(joinResponse.getUserId());
+        User byId = userService.findById(joinResponse.getUserId());
 
-        assertEquals(joinResponse.getNickname(), userInfo.getNickname());
-        assertEquals(joinResponse.getRegion(), userInfo.getRegion());
-        assertEquals(joinResponse.getBirthDate(), userInfo.getBirthDate());
-        assertEquals(joinResponse.getSelfIntroduction(), userInfo.getSelfIntroduction());
+        assertEquals(joinResponse.getNickname(), byId.getNickname());
+        assertEquals(joinResponse.getRegion(), byId.getIntroduction().getRegion());
+        assertEquals(joinResponse.getBirthDate(), byId.getIntroduction().getBirthDate());
+        assertEquals(joinResponse.getSelfIntroduction(), byId.getIntroduction().getSelfIntroduction());
     }
 
     @Test
