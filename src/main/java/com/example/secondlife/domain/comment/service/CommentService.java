4d5ae8c54b04959953bc755d1bf46d4cf8164f5e
aammddkkzxc <ideal0427@naver.com>
2024-03-28 20:16:16 +0900

    refactor : #29 댓글 단건 조회 메서드 순서 변경

--- src/main/java/com/example/secondlife/domain/comment/service/CommentService.java
+++ src/main/java/com/example/secondlife/domain/comment/service/CommentService.java
@@ -3,7 +3,6 @@ package com.example.secondlife.domain.comment.service;
 import com.example.secondlife.domain.comment.dto.CommentRequest;
 import com.example.secondlife.domain.comment.dto.CommentResponse;
 import com.example.secondlife.domain.comment.dto.CommentUpdateRequest;
-import com.example.secondlife.domain.comment.dto.CommentsResponse;
 import com.example.secondlife.domain.comment.entity.Comment;
 import com.example.secondlife.domain.comment.repository.CommentRepository;
 import com.example.secondlife.domain.post.entity.Post;
@@ -11,8 +10,6 @@ import com.example.secondlife.domain.post.service.PostService;
 import com.example.secondlife.domain.user.entity.User;
 import com.example.secondlife.domain.user.service.UserService;
 import jakarta.transaction.Transactional;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -36,12 +33,6 @@ public class CommentService {
         return savedComment.toCommentResponse();
     }
 
-
-    public Comment findCommentById(Long commentId) {
-        return commentRepository.findById(commentId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다. commentId = " + commentId));
-    }
-
     public CommentResponse updateComment(Long commentId, Long userId, CommentUpdateRequest request) {
         Comment findComment = findCommentById(commentId);
 
@@ -53,6 +44,11 @@ public class CommentService {
         // 현재 사용자가 코멘트의 작성자와 동일한 경우
         findComment.update(request);
         return findComment.toCommentResponse();
+    }
+
+    public Comment findCommentById(Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다. commentId = " + commentId));
     }
 
     public void deleteComment(Long commentId, Long userId) {
