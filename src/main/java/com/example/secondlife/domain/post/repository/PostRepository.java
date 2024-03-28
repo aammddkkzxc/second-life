47d114a7158e76377d905b5fb98d227aafd2db81
js <2kpppk@gmail.com>
2024-03-28 16:44:52 +0900

    rename : member_id - > user_id, 아직 안쓰는 코드 주석처리

--- src/main/java/com/example/secondlife/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/secondlife/domain/post/repository/PostRepository.java
@@ -1,13 +1,11 @@
 package com.example.secondlife.domain.post.repository;
 
 import com.example.secondlife.domain.post.entity.Post;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("select p from Post p left join fetch p.comments")
-    List<Post> findAllWithComments();
+//    @Query("select p from Post p left join fetch p.comments")
+//    List<Post> findAllWithComments();
 
 }
