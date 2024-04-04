bbc3e6f456d2bd3d662e82b685b2088cdef0e4ab
aammddkkzxc <ideal0427@naver.com>
2024-04-04 19:42:50 +0900

    feat : 테스트 초안

---
+++ src/test/java/com/example/secondlife/domain/BasicCRUDTest.java
@@ -0,0 +1,51 @@
+package com.example.secondlife.domain;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BasicCRUDTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected <T> ResultActions doPost(String url, T request) throws Exception {
+        return mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        );
+    }
+
+    protected ResultActions doGet(String url, Long id) throws Exception {
+        return mockMvc.perform(get(url, id)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+    }
+
+    protected <T> ResultActions doPatch(String url, Long id, T request) throws Exception {
+        return mockMvc.perform(patch(url, id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        );
+    }
+
+    protected ResultActions doDelete(String url, Long id) throws Exception {
+        return mockMvc.perform(delete(url, id)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+    }
+}
