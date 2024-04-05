0b4f7754b2651e53fd2fdafaf684f353007a41c8
aammddkkzxc <ideal0427@naver.com>
2024-04-05 20:16:41 +0900

    test : userRepo 테스트

--- src/test/java/com/example/secondlife/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/com/example/secondlife/domain/user/repository/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.secondlife.domain.user.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.example.secondlife.domain.user.entity.User;
@@ -33,6 +34,7 @@ class UserRepositoryTest {
                 .loginId("idExample")
                 .nickname("test")
                 .password("1234")
+                .email("test@example.com")
                 .introduction(new Introduction(Region.CHUNGNAM, localDateEx, "hello"))
                 .role(Role.L1)
                 .build();
@@ -69,6 +71,62 @@ class UserRepositoryTest {
 
         //when
         Optional<User> optionalUser = userRepository.findByLoginId(loginId);
+
+        //then
+        assertTrue(optionalUser.isEmpty());
+    }
+
+    @DisplayName("이메일이 존재하는 경우 true를 반환")
+    @Test
+    void existsByEmail_ExistingEmail() {
+        //given
+        String email = "test@example.com";
+
+        //when
+        boolean exists = userRepository.existsByEmail(email);
+
+        //then
+        assertTrue(exists);
+    }
+
+    @DisplayName("이메일이 존재하지 않는 경우 false를 반환")
+    @Test
+    void existsByEmail_NonExistingEmail() {
+        //given
+        String email = "nonexisting@example.com";
+
+        //when
+        boolean exists = userRepository.existsByEmail(email);
+
+        //then
+        assertFalse(exists);
+    }
+
+    @DisplayName("존재하는 닉네임으로 유저를 찾을 시 해당 유저 반환")
+    @Test
+    void findByNicknameContaining_ExistingNickname() {
+        //given
+        String nickname = "test";
+
+        //when
+        Optional<User> optionalUser = userRepository.findByNicknameContaining(nickname);
+
+        //then
+
+        assertAll(
+                () -> assertTrue(optionalUser.isPresent()),
+                () -> assertThat(user).isEqualTo(optionalUser.get())
+        );
+    }
+
+    @DisplayName("존재하지 않는 닉네임으로 유저를 찾을 시 빈 Optional 반환")
+    @Test
+    void findByNicknameContaining_NonExistingNickname() {
+        //given
+        String partialNickname = "nonExisting";
+
+        //when
+        Optional<User> optionalUser = userRepository.findByNicknameContaining(partialNickname);
 
         //then
         assertTrue(optionalUser.isEmpty());
