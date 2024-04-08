6bf54870689789e403bf939bfaafc4b102bc21bb
aammddkkzxc <ideal0427@naver.com>
2024-04-08 11:38:44 +0900

    teat : post test

--- src/test/java/com/example/secondlife/domain/post/controller/api/PostControllerTest.java
+++ src/test/java/com/example/secondlife/domain/post/controller/api/PostControllerTest.java
@@ -5,37 +5,31 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.example.secondlife.domain.BasicCRUDTest;
+import com.example.secondlife.BasicCRUDTest;
 import com.example.secondlife.domain.post.dto.PostDto;
 import com.example.secondlife.domain.post.dto.PostResponse;
 import com.example.secondlife.domain.post.entity.Post;
 import com.example.secondlife.domain.post.enumType.Forum;
 import com.example.secondlife.domain.post.service.PostService;
 import com.example.secondlife.domain.user.entity.User;
-import com.example.secondlife.domain.user.repository.UserRepository;
 import com.example.secondlife.domain.user.service.UserSearchService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.ResultActions;
 
 public class PostControllerTest extends BasicCRUDTest {
 
-
-    @Mock
+    @MockBean
     private PostService postService;
 
-    @Mock
+    @MockBean
     private UserSearchService userSearchService;
-
-    @Autowired
-    UserRepository userRepository;
 
     @DisplayName("게시글 작성 요청 테스트")
     @Test
     void writePostTest() throws Exception {
-
+        //given
         Long userId = 123L;
         PostDto request = PostDto.builder().forum(Forum.FREE).build();
         String url = "/posts";
@@ -44,13 +38,14 @@ public class PostControllerTest extends BasicCRUDTest {
                 postResponse);
         Post post = postRequestToPost(userId, request);
 
+        //when
         ResultActions result = doPost(url, post);
 
+        //then
         assertAll(
                 () -> result.andExpect(status().is3xxRedirection()),
                 () -> verify(postService).save(userId, request)
         );
-
     }
 
     private Post postRequestToPost(Long userId, PostDto request) {
